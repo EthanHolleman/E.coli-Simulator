ed697f1c7d5bc85242b2164b9ad27cc746aeafae
Ethan Holleman <eholleman@luc.edu>
2019-03-09 16:46:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-def main():
+from gen import baseComp, createMatrix, simulate
 
+def main():
+  sums = baseComp("GMR30.txt")
+  dict = createMatrix("GMR30.txt")
+
+  seq = simulate(dict, 10000, sums)
+  out = ""
+  for i in range(1,len(seq)):
+     letter = str(seq[i])
+     letter = letter[2]
+     out = out + letter
+
+print(out)
 
 if __name__ == "__main__": main()
