bec0c2dc58ad34f6915d8ae57e121e30a6fdcce6
Ethan Holleman <eholleman@luc.edu>
2019-03-16 20:00:32 -0500

    Update gen.py
    Overall of the way the transition matrix is generated which makes it easier for the simulate functions to interpret

--- gen.py
+++ gen.py
@@ -27,57 +27,58 @@ def baseComp(filePath): #calculates base composition of a files
 def createMatrix(filePath):
     #creates a dictionary which will contains the conditional probility of
     #any given two letter word occuring
-    dict = {}
-    count = 0
+    refDict = {'A':0, 'T':1, 'G':2, 'C':3}
+
+    w, h = 4, 4;
+    Matrix = [[0 for x in range(w)] for y in range(h)]
+
     with open(filePath) as seqFile: #only reading one sequence from fasta currently
         seqFile.readline()
         line = seqFile.readline()
-        line = line.strip()
 
         while line:
-
             for i in range(len(line)-1):
-                word = line[i] + line[i+1]
 
-                if word in dict:
-                    dict[word] +=1
-                    count += 1
-                else:
-                    dict[word] = 1
-                    count += 1
+                try:
+                    row = refDict[line[i]]
+                    column = refDict[line[i+1]]
+                    Matrix[row][column] += 1
+                except KeyError:
+                    pass
 
             seqFile.readline()
             line = seqFile.readline()
-            line = line.strip()
 
-    for word in dict:
-        dict[word] = dict[word] / count
-    print(count)
-    return dict
+        #still need to divide by row totals for each cell of matrix
+        for row in Matrix:
+            rowTotal = sum(row)
+            for i in range(0,4):
+                row[i] /=rowTotal
+            #print(sum(row)) check role totals == 1
+
+    return Matrix
 
 
 def simulate(theMatrix, lengthSim, baseComposition):
     #Simulates a given length of e. Coli genome and returns the sequence
     #theMatrix should be dictionary from createMatrix
 
-    bases = ['A', 'T', 'G', 'C'] #list of canidates
+    bases = [0,1,2,3] #list of canidates
+    refDict = {'A':0, 'T':1, 'G':2, 'C':3}
     pi = baseComposition #first matrix based on A, T, G, C comps
-    combos = []
-    seq = []
 
     firstBase = str(np.random.choice(bases,1,pi))
     seq.append(firstBase)
 
     for i in range(0,lengthSim):
+        try:
+            row = refDict[seq[i]] #takes current base in seq converts to int key
+        except:
+            pass
 
-        current = str(seq[i])
-        current = current[2]
-        combos = [current + letter for letter in bases]
-        pi = [theMatrix[x] for x in combos]
-
-        next = str(np.random.choice(bases,1,pi))
+        pi = [theMatrix[row][x] for x in bases] #conditional probs for each row
+        next = str(np.random.choice(bases,1,pi)) #pics 1 base from all possible
         seq.append(next)
-
 
     return seq
 
@@ -88,3 +89,4 @@ def bulkSim(theMatrix, lengthSim, baseComposition, numSims, runName):
 
         with open("output.fasta", "a") as out:
             out.write("< " + runName + "Run: " + i)
+
