c92665ea82c1203f366aab53baf03dcc3466878e
Ethan Holleman <eholleman@luc.edu>
2019-03-18 17:27:37 -0500

    Update gen.py
    simulate method actually works now

--- gen.py
+++ gen.py
@@ -49,36 +49,28 @@ def createMatrix(filePath):
             seqFile.readline()
             line = seqFile.readline()
 
-        #still need to divide by row totals for each cell of matrix
         for row in Matrix:
             rowTotal = sum(row)
             for i in range(0,4):
                 row[i] /=rowTotal
-            #print(sum(row)) check role totals == 1
 
     return Matrix
 
 
 def simulate(theMatrix, lengthSim, baseComposition):
     #Simulates a given length of e. Coli genome and returns the sequence
-    #theMatrix should be dictionary from createMatrix
-
-    bases = [0,1,2,3] #list of canidates
+    bases = ["A","T","G","C"] #list of canidates
     refDict = {'A':0, 'T':1, 'G':2, 'C':3}
     pi = baseComposition #first matrix based on A, T, G, C comps
+    seq = []
 
-    firstBase = str(np.random.choice(bases,1,pi))
-    seq.append(firstBase)
+    firstBase = np.random.choice(bases,1,pi) #starts the chain
+    seq.append(firstBase.item(0))
 
     for i in range(0,lengthSim):
-        try:
-            row = refDict[seq[i]] #takes current base in seq converts to int key
-        except:
-            pass
-
-        pi = [theMatrix[row][x] for x in bases] #conditional probs for each row
-        next = str(np.random.choice(bases,1,pi)) #pics 1 base from all possible
-        seq.append(next)
+        pi = theMatrix[refDict[seq[i]]] #reassigns prob for base at current position
+        next = (np.random.choice(bases,1,p=pi))
+        seq.append(next.item(0))
 
     return seq
 
@@ -89,4 +81,3 @@ def bulkSim(theMatrix, lengthSim, baseComposition, numSims, runName):
 
         with open("output.fasta", "a") as out:
             out.write("< " + runName + "Run: " + i)
-
